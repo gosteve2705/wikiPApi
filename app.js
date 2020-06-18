60b2ab2689db08013f55cc157006a89c9a8a2c5a
gosteve <nomuojamaro@gmail.com>
2020-06-18 11:15:08 +0100

    added feature to GET,PUT and PATCH specific articles

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -37,7 +38,7 @@ app.route("/articles")
 .post(function (req,res){
 
   const newArticle = new Article({
-  title:  req.body.title,
+  title:  _.capitalize(req.body.title),
   content: req.body.content
   });
 
@@ -63,7 +64,42 @@ app.route("/articles")
 });
 
 
-
+app.route("/articles/:title")
+.get(function(req,res){
+  Article.findOne({title: _.capitalize(req.params.title) }, function(err,foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    }else{
+      res.send("No article matching that title was found");
+    }
+  });
+})
+.put(function(req,res){
+  Article.update(
+    {title: _.capitalize(req.params.title) },
+    {title: _.capitalize(req.body.title), content: req.body.content},
+    {overwrite:true},
+    function(err){
+      if (!err){
+        res.send('successfully updated the article');
+      }
+    }
+  );
+})
+.patch(function(req,res){
+  Article.update(
+    {title:_.capitalize(req.params.title)},
+    {$set :req.body},
+    function(err){
+      if(!err){
+        res.send("successfully updated the article");
+      }else{
+        res.send(err);
+      }
+    }
+  );
+})
+.delete();
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");

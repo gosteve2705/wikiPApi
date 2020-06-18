1b972b58d3dc443c6ca6edcb40b8dc5beba9ca1b
gosteve <nomuojamaro@gmail.com>
2020-06-18 01:09:13 +0100

    first commit -  server setup, database added , GET,POST,DELETE articles requests handled

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+// mongoose.connect('mongodb+srv://gosteve:chemistry@cluster0-dzckd.mongodb.net/todoListDB?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+const wikiSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model('article', wikiSchema);
+
+app.route("/articles")
+.get(function(req, res) {
+  Article.find({},function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+
+  });
+})
+.post(function (req,res){
+
+  const newArticle = new Article({
+  title:  req.body.title,
+  content: req.body.content
+  });
+
+  newArticle.save(
+    function(err){
+      if(!err){
+        res.send('successfully added new article');
+      }else{
+        res.send(err);
+      }
+    }
+  );
+  res.redirect('/articles');
+})
+.delete(function (req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("successfully deleted all the articles");
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
